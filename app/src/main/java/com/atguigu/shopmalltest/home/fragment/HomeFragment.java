48f091d4eb488dc424766835286ef1656bc3eaf4
lianshuideyu <834893479@qq.com>
2017-06-12 15:24:31 +0800

    分析适配器的实现；；设置横幅广播的适配器

--- app/src/main/java/com/atguigu/shopmalltest/home/fragment/HomeFragment.java
+++ app/src/main/java/com/atguigu/shopmalltest/home/fragment/HomeFragment.java
@@ -1,16 +1,26 @@
 package com.atguigu.shopmalltest.home.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.atguigu.shopmalltest.R;
 import com.atguigu.shopmalltest.base.BaseFragment;
+import com.atguigu.shopmalltest.home.adapter.HomeAdapter;
 import com.atguigu.shopmalltest.home.bean.HomeBean;
 import com.atguigu.shopmalltest.until.Constants;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import okhttp3.Call;
 
 /**
@@ -19,25 +29,35 @@ import okhttp3.Call;
 
 public class HomeFragment extends BaseFragment {
     private static final String TAG = HomeFragment.class.getSimpleName();//"HomeFragment"
+    @BindView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @BindView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @BindView(R.id.rv_home)
+    RecyclerView rvHome;
+    @BindView(R.id.ib_top)
+    ImageButton ibTop;
+    Unbinder unbinder;
     private String homeUrl;
 
+    private HomeAdapter adapter;
     /**
      * 初始化控件
      * retur
      */
     @Override
     public View initView() {
-        Log.e(TAG,"初始化主页控件...");
+        Log.e(TAG, "初始化主页控件...");
 
-        View view =  View.inflate(mContext, R.layout.fragment_home,null);
-
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        Log.e(TAG,"绑定数据到控件上...");
+        Log.e(TAG, "绑定数据到控件上...");
 
         getDataFromNet();
     }
@@ -52,16 +72,32 @@ public class HomeFragment extends BaseFragment {
 
     }
 
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(mContext, "消息通知", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_top:
+                Toast.makeText(mContext, "回到顶部", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
     private class MyStringCallback extends StringCallback {
 
         @Override
         public void onError(Call call, Exception e, int id) {
-            Log.e(TAG,"联网失败==" + e.getMessage());
+            Log.e(TAG, "联网失败==" + e.getMessage());
         }
 
         @Override
         public void onResponse(String response, int id) {
-            Log.e(TAG,"联网成功." + response);
+            Log.e(TAG, "联网成功." + response);
             //解析数据
             processData(response);
         }
@@ -71,6 +107,18 @@ public class HomeFragment extends BaseFragment {
 
         HomeBean homeBean = JSON.parseObject(json, HomeBean.class);
 
-        Log.e(TAG,"解析成功==" + homeBean.getResult().getAct_info().get(0).getName());
+        Log.e(TAG, "解析成功==" + homeBean.getResult().getAct_info().get(0).getName());
+
+        adapter = new HomeAdapter(mContext,homeBean.getResult());
+
+        LinearLayoutManager manager = new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false);
+        rvHome.setLayoutManager(manager);
+        rvHome.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
